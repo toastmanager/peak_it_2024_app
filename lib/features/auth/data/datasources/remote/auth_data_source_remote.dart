5f03a905d2b95bcc5ee889c567451cc87f9658cd
toastmanager <mrbonkyou@gmail.com>
2024-10-15 16:15:20 +0900

    feat(auth): auth remote data source

---
+++ lib/features/auth/data/datasources/remote/auth_data_source_remote.dart
@@ -0,0 +1,24 @@
+import 'package:injectable/injectable.dart';
+import 'package:peak_it_2024_app/features/auth/data/models/sing_in_model.dart';
+import 'package:peak_it_2024_app/features/auth/data/models/sing_up_model.dart';
+
+abstract class AuthDataSourceRemote {
+  Future<void> signIn(SignInModel model);
+  Future<void> signUp(SignUpModel model);
+}
+
+@Singleton(as: AuthDataSourceRemote)
+class AuthDataSourceRemoteImpl implements AuthDataSourceRemote {
+  @override
+  Future<void> signIn(SignInModel model) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp(SignUpModel model) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+  
+}
